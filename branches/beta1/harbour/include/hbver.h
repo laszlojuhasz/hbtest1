954415740d41c49250e15c54dde4fc6848331933
druzus <druzus@ef52b332-12a4-b543-bf59-08b57769829b>
2007-01-09 14:57:42 +0000

    2007-01-09 15:57 UTC+0100 Przemyslaw Czerpak (druzus/at/priv.onet.pl)   * harbour/ChangeLog   * harbour/harbour.spec   * harbour/include/hbver.h     * created branch tag "beta1"     * updated version number to 0.99.0
    git-svn-id: http://harbour-project.svn.sourceforge.net/svnroot/harbour-project@6971 ef52b332-12a4-b543-bf59-08b57769829b

--- branches/beta1/harbour/include/hbver.h
+++ branches/beta1/harbour/include/hbver.h
@@ -56,13 +56,13 @@
 /* NOTE: One of these next three fields can be incremented by the hbverfix program */
 
 #define HB_VER_MAJOR    0       /* Major version number */
-#define HB_VER_MINOR    47      /* Minor version number */
+#define HB_VER_MINOR    99      /* Minor version number */
 #define HB_VER_REVISION 0       /* Revision number */
 
 /* NOTE: The next two fields are automatically updated by the hbverfix program */
 
-#define HB_VER_LENTRY   "2007-01-09 15:34 UTC+0100 Przemyslaw Czerpak"
-#define HB_VER_CHLCVS   "ChangeLog,v 1.5168 2007/01/09 14:34:33 druzus"
+#define HB_VER_LENTRY   "2007-01-09 15:57 UTC+0100 Przemyslaw Czerpak"
+#define HB_VER_CHLCVS   "ChangeLog,v 1.5169 2007/01/09 15:57:00 druzus"
 
 /* TOFIX: The next three fields need to get updated automatically */
 
