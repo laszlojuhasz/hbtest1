laszlojuhasz/hbtest1

commit 7b5c11f4caef395332c12d291fba150dc1fb3003

2012-11-07 03:28 UTC+0100 Viktor Szakats (harbour syenar.net)   * contrib/hbnf/hbnf.hbx   * contrib/hbodbc/hbodbc.hbx   * contrib/hbtip/hbtip.hbx   * contrib/hbtpathy/hbtpathy.hbx   * contrib/xhb/xhb.hbx     ! minor fixes
  * contrib/hblzf/tests/*.prg
  * contrib/hbmxml/format.prg
  * contrib/hbmysql/tests/test.prg
  * contrib/hbmysql/tmysql.prg
  * contrib/hbmysql/tsqlbrw.prg
  * contrib/hbnetio/netiomt.prg
  * contrib/hbnetio/utils/hbnetio/*.prg
  * contrib/hbnf/*.prg
  * contrib/hbnf/tests/*.prg
  * contrib/hbodbc/tests/testodbc.prg
  * contrib/hbodbc/todbc.prg
  * contrib/hbpgsql/tests/*.prg
  * contrib/hbpgsql/tpostgre.prg
  * contrib/hbssl/tests/*.prg
  * contrib/hbtip/*.prg
  * contrib/hbtip/tests/*.prg
  * contrib/hbtpathy/telepath.prg
  * contrib/hbzebra/tests/*.prg
  * contrib/rddads/tests/*.prg
  * contrib/xhb/*.prg
  * contrib/xhb/tests/*.prg
    * formatted
    ! commented code converted to #if 0/#endif block



git-svn-id: http://harbour-project.svn.sourceforge.net/svnroot/harbour-project@18462 ef52b332-12a4-b543-bf59-08b57769829b

trunk/harbour/contrib/hbtip/mail.prg
@@ -139,7 +139,7 @@ METHOD New( cBody, oEncoder ) CLASS TipMail
 METHOD SetEncoder( cEncoder ) CLASS TipMail
 
    IF HB_ISSTRING( cEncoder )
-      ::oEncoder := TIp_GetEncoder( cEncoder )
+      ::oEncoder := tip_GetEncoder( cEncoder )
    ELSE
       ::oEncoder := cEncoder
    ENDIF
@@ -438,7 +438,7 @@ METHOD FromString( cMail, cBoundary, nPos ) CLASS TipMail
    // boundary.
 
    IF "Content-Transfer-Encoding" $ ::hHeaders
-      ::oEncoder := TIp_GetEncoder( ::hHeaders[ "Content-Transfer-Encoding" ] )
+      ::oEncoder := tip_GetEncoder( ::hHeaders[ "Content-Transfer-Encoding" ] )
    ENDIF
 
    // se if we have subparts:
@@ -483,7 +483,7 @@ METHOD FromString( cMail, cBoundary, nPos ) CLASS TipMail
          ENDIF
 
          // Add our subsection
-         oSubSection := TipMail():New()
+         oSubSection := TIPMail():New()
          nPos := oSubSection:FromString( cMail, cSubBoundary, ;
             nLinePos + 2 )
 
@@ -567,14 +567,14 @@ METHOD setHeader( cSubject, cFrom, xTo, xCC, xBCC ) CLASS TipMail
       RETURN .F.
    ENDIF
 
-   IF ! ::setFieldPart( "From", LTrim( WordEncodeQ( tip_GetNameEMail( AllTrim( cFrom ) ), ::cCharset ) + " <" + tip_GetRawEMail( AllTrim( cFrom ) ) + ">" ) )
+   IF ! ::setFieldPart( "From", LTrim( WordEncodeQ( tip_GetNameEmail( AllTrim( cFrom ) ), ::cCharset ) + " <" + tip_GetRawEmail( AllTrim( cFrom ) ) + ">" ) )
       RETURN .F.
    ENDIF
 
    cTo := ""
    imax := Len( aTO )
    FOR i := 1 TO imax
-      cTo += LTrim( WordEncodeQ( tip_GetNameEMail( AllTrim( aTo[ i ] ) ), ::cCharset ) + " <" + tip_GetRawEMail( AllTrim( aTo[ i ] ) ) + ">" )
+      cTo += LTrim( WordEncodeQ( tip_GetNameEmail( AllTrim( aTo[ i ] ) ), ::cCharset ) + " <" + tip_GetRawEmail( AllTrim( aTo[ i ] ) ) + ">" )
       IF i < imax
          cTo += "," + tip_CRLF() + " "
       ENDIF
@@ -592,7 +592,7 @@ METHOD setHeader( cSubject, cFrom, xTo, xCC, xBCC ) CLASS TipMail
       cCC := ""
       imax := Len( aCC )
       FOR i := 2 TO imax
-         cCC += LTrim( WordEncodeQ( tip_GetNameEMail( AllTrim( aCC[ i ] ) ), ::cCharset ) + " <" + tip_GetRawEMail( AllTrim( aCC[ i ] ) ) + ">" )
+         cCC += LTrim( WordEncodeQ( tip_GetNameEmail( AllTrim( aCC[ i ] ) ), ::cCharset ) + " <" + tip_GetRawEmail( AllTrim( aCC[ i ] ) ) + ">" )
          IF i < imax
             cCC += "," + tip_CRLF() + " "
          ENDIF
@@ -607,7 +607,7 @@ METHOD setHeader( cSubject, cFrom, xTo, xCC, xBCC ) CLASS TipMail
       cBCC := ""
       imax := Len( aBCC )
       FOR i := 2 TO imax
-         cBCC += LTrim( WordEncodeQ( tip_GetNameEMail( AllTrim( aBCC[ i ] ) ), ::cCharset ) + " <" + tip_GetRawEMail( AllTrim( aBCC[ i ] ) ) + ">" )
+         cBCC += LTrim( WordEncodeQ( tip_GetNameEmail( AllTrim( aBCC[ i ] ) ), ::cCharset ) + " <" + tip_GetRawEmail( AllTrim( aBCC[ i ] ) ) + ">" )
          IF i < imax
             cBCC += "," + tip_CRLF() + " "
          ENDIF
@@ -623,7 +623,7 @@ METHOD setHeader( cSubject, cFrom, xTo, xCC, xBCC ) CLASS TipMail
 METHOD attachFile( cFileName ) CLASS TipMail
 
    LOCAL cContent := hb_MemoRead( cFileName )
-   LOCAL cMimeType := TIP_FileMimetype( cFileName )
+   LOCAL cMimeType := tip_FileMimeType( cFileName )
    LOCAL cDelim := hb_ps()
 
    LOCAL oAttach
@@ -730,7 +730,7 @@ STATIC FUNCTION WordEncodeQ( cData, cCharset )
 
    RETURN iif( lToEncode, cString + "?=", cData )
 
-FUNCTION tip_GetRawEMail( cAddress )
+FUNCTION tip_GetRawEmail( cAddress )
 
    LOCAL tmp, tmp1
 
@@ -742,7 +742,7 @@ FUNCTION tip_GetRawEMail( cAddress )
 
    RETURN cAddress
 
-FUNCTION tip_GetNameEMail( cAddress )
+FUNCTION tip_GetNameEmail( cAddress )
 
    LOCAL tmp
 

commit 0597a0e35690ce30c5585acb47e0fe8e1e2134b6

2012-11-21 03:35 UTC+0100 Viktor Szakats (harbour syenar.net)   * contrib/hbtip/mail.prg     ! fixed regression in r18462 where casing of class name       in a string got accidentally converted to natural.       Many thanks to Itamar Lins for the find.
git-svn-id: http://harbour-project.svn.sourceforge.net/svnroot/harbour-project@18622 ef52b332-12a4-b543-bf59-08b57769829b

trunk/harbour/contrib/hbtip/mail.prg
@@ -251,7 +251,7 @@ METHOD SetFieldOption( cPart, cOption, cValue ) CLASS TIPMail
 
 METHOD Attach( oSubPart ) CLASS TIPMail
 
-   IF HB_ISOBJECT( oSubPart ) .AND. oSubPart:ClassName == "TIPMail"
+   IF HB_ISOBJECT( oSubPart ) .AND. oSubPart:ClassName == "TIPMAIL"
       // reset wrong content-type
       IF At( "multipart/", Lower( ::GetFieldPart( "Content-Type" ) ) ) == 0
          ::hHeaders[ "Content-Type" ] := "multipart/mixed"